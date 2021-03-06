36a3aaef4fe1554acf2cfcf28d30e606a76a776a
tolani <D00230487@student.dkit.ie>
2021-03-06 16:54:20 +0000

    Used google font styles for body and header

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,8 @@
 <!-- the head section -->
 <head>
 <title>My PHP CRUD App</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/mystyle.css">
 </head>
 
