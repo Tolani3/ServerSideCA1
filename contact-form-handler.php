03090971dd540fddc4c98451f3297cfb16255086
tolani <D00230487@student.dkit.ie>
2021-03-18 19:53:13 +0000

    Added PhoneNumber field with validation to contatct form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,6 +4,7 @@ $myemail = 'd00230487@student.dkit.ie'; //<-----Put your DkIT email address here
 if (
 	empty($_POST['name'])  ||
 	empty($_POST['email']) ||
+	empty($_POST['phoneNumber']) ||
 	empty($_POST['message'])
 ) {
 	$errors .= "\n Error: all fields are required";
@@ -11,6 +12,7 @@ if (
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
+$phone_number = $_POST['phoneNumber'];
 $message = $_POST['message'];
 
 if (!preg_match(
@@ -20,18 +22,25 @@ if (!preg_match(
 	$errors .= "\n Error: Invalid email address";
 }
 
+if (!preg_match(
+	"/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/",
+	$phone_number
+)) {
+	$errors .= "\n Invalid Number!";
+}
+
 if (empty($errors)) {
 	$to = $myemail;
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. " .
-		" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
+		"Here are the details:\n Name: $name \n Email: $email_address \n PhoneNumber: $phone_number \n Message \n $message";
 
 	$headers = "From: $myemail\n";
 	$headers .= "Reply-To: $email_address";
 
 	mail($to, $email_subject, $email_body, $headers);
 	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
+	header('Location: contact-form-thank-you.php');
 }
 ?>
 <!DOCTYPE HTML>
--- contact.php
+++ contact.php
@@ -9,14 +9,22 @@
             <label for='name'>Your Name:</label> <br>
             <input type="text" name="name">
         </p>
+
         <p>
             <label for='email'> Email Address:</label><br>
             <input type="text" name="email">
         </p>
+
+        <p>
+            <label for='phoneNumber'> Phone Number:</label><br>
+            <input type="text" name="phoneNumber">
+        </p>
+
         <p>
             <label for='message'> Message:</label><br>
             <textarea name="message"></textarea>
         </p>
+
         <input type="submit" value="Submit"><br>
     </form>
     <?php
